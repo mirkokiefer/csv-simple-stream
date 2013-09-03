2f70f58a3c3ef09942a713f92b1d6d5ee295b125
Mirko Kiefer <mail@mirkokiefer.com>
2013-09-03 09:51:25 +0200

    tidy

--- index.js
+++ index.js
@@ -6,63 +6,69 @@ var iterators = require('async-iterators')
 The csv line parsing code is thankfully taken from @ridgerunner's excellent stackoverflow answer:
 http://stackoverflow.com/a/8497474/1099982
 
+The commented regular expression for readability:
+
+re_valid = r"""
+# Validate a CSV string having single, double or un-quoted values.
+^                                   # Anchor to start of string.
+\s*                                 # Allow whitespace before value.
+(?:                                 # Group for value alternatives.
+  '[^'\\]*(?:\\[\S\s][^'\\]*)*'     # Either Single quoted string,
+| "[^"\\]*(?:\\[\S\s][^"\\]*)*"     # or Double quoted string,
+| [^,'"\s\\]*(?:\s+[^,'"\s\\]+)*    # or Non-comma, non-quote stuff.
+)                                   # End group of value alternatives.
+\s*                                 # Allow whitespace after value.
+(?:                                 # Zero or more additional values
+  ,                                 # Values separated by a comma.
+  \s*                               # Allow whitespace before value.
+  (?:                               # Group for value alternatives.
+    '[^'\\]*(?:\\[\S\s][^'\\]*)*'   # Either Single quoted string,
+  | "[^"\\]*(?:\\[\S\s][^"\\]*)*"   # or Double quoted string,
+  | [^,'"\s\\]*(?:\s+[^,'"\s\\]+)*  # or Non-comma, non-quote stuff.
+  )                                 # End group of value alternatives.
+  \s*                               # Allow whitespace after value.
+)*                                  # Zero or more additional values
+$                                   # Anchor to end of string.
+"""
+
+re_value = r"""
+# Match one value in valid CSV string.
+(?!\s*$)                            # Don't match empty last value.
+\s*                                 # Strip whitespace before value.
+(?:                                 # Group for value alternatives.
+  '([^'\\]*(?:\\[\S\s][^'\\]*)*)'   # Either $1: Single quoted string,
+| "([^"\\]*(?:\\[\S\s][^"\\]*)*)"   # or $2: Double quoted string,
+| ([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)  # or $3: Non-comma, non-quote stuff.
+)                                   # End group of value alternatives.
+\s*                                 # Strip whitespace after value.
+(?:,|$)                             # Field ends on comma or EOS.
+"""
+
 */
 
-// re_valid = r"""
-// # Validate a CSV string having single, double or un-quoted values.
-// ^                                   # Anchor to start of string.
-// \s*                                 # Allow whitespace before value.
-// (?:                                 # Group for value alternatives.
-//   '[^'\\]*(?:\\[\S\s][^'\\]*)*'     # Either Single quoted string,
-// | "[^"\\]*(?:\\[\S\s][^"\\]*)*"     # or Double quoted string,
-// | [^,'"\s\\]*(?:\s+[^,'"\s\\]+)*    # or Non-comma, non-quote stuff.
-// )                                   # End group of value alternatives.
-// \s*                                 # Allow whitespace after value.
-// (?:                                 # Zero or more additional values
-//   ,                                 # Values separated by a comma.
-//   \s*                               # Allow whitespace before value.
-//   (?:                               # Group for value alternatives.
-//     '[^'\\]*(?:\\[\S\s][^'\\]*)*'   # Either Single quoted string,
-//   | "[^"\\]*(?:\\[\S\s][^"\\]*)*"   # or Double quoted string,
-//   | [^,'"\s\\]*(?:\s+[^,'"\s\\]+)*  # or Non-comma, non-quote stuff.
-//   )                                 # End group of value alternatives.
-//   \s*                               # Allow whitespace after value.
-// )*                                  # Zero or more additional values
-// $                                   # Anchor to end of string.
-// """
-
-// re_value = r"""
-// # Match one value in valid CSV string.
-// (?!\s*$)                            # Don't match empty last value.
-// \s*                                 # Strip whitespace before value.
-// (?:                                 # Group for value alternatives.
-//   '([^'\\]*(?:\\[\S\s][^'\\]*)*)'   # Either $1: Single quoted string,
-// | "([^"\\]*(?:\\[\S\s][^"\\]*)*)"   # or $2: Double quoted string,
-// | ([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)  # or $3: Non-comma, non-quote stuff.
-// )                                   # End group of value alternatives.
-// \s*                                 # Strip whitespace after value.
-// (?:,|$)                             # Field ends on comma or EOS.
-// """
+var parseLine = function(text) {
+  var re_valid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/
+  var re_value = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g
+  // Return NULL if input string is not well formed CSV string.
+  if (!re_valid.test(text)) return null
+  var a = [] // Initialize array to receive values.
+  text.replace(re_value, // "Walk" the string using replace with callback.
+    function(m0, m1, m2, m3) {
+        // Remove backslash from \' in single quoted values.
+        if      (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"))
+        // Remove backslash from \" in double quoted values.
+        else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'))
+        else if (m3 !== undefined) a.push(m3)
+        return '' // Return empty string.
+    })
+  // Handle special case of empty last value.
+  if (/,\s*$/.test(text)) a.push('')
+  return a
+}
 
 var createCSVIterator = function(lineIterator) {
   return iterators.map(lineIterator, function(err, text) {
-    var re_valid = /^\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*(?:,\s*(?:'[^'\\]*(?:\\[\S\s][^'\\]*)*'|"[^"\\]*(?:\\[\S\s][^"\\]*)*"|[^,'"\s\\]*(?:\s+[^,'"\s\\]+)*)\s*)*$/
-    var re_value = /(?!\s*$)\s*(?:'([^'\\]*(?:\\[\S\s][^'\\]*)*)'|"([^"\\]*(?:\\[\S\s][^"\\]*)*)"|([^,'"\s\\]*(?:\s+[^,'"\s\\]+)*))\s*(?:,|$)/g
-    // Return NULL if input string is not well formed CSV string.
-    if (!re_valid.test(text)) return null
-    var a = [] // Initialize array to receive values.
-    text.replace(re_value, // "Walk" the string using replace with callback.
-      function(m0, m1, m2, m3) {
-          // Remove backslash from \' in single quoted values.
-          if      (m1 !== undefined) a.push(m1.replace(/\\'/g, "'"))
-          // Remove backslash from \" in double quoted values.
-          else if (m2 !== undefined) a.push(m2.replace(/\\"/g, '"'))
-          else if (m3 !== undefined) a.push(m3)
-          return '' // Return empty string.
-      })
-    // Handle special case of empty last value.
-    if (/,\s*$/.test(text)) a.push('')
-    return a
+    return parseLine(text)
   })
 }
 
